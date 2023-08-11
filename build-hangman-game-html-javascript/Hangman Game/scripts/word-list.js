19a39f930c4a78182d9dd0c7603107e14403fef7
kikiwongweb <wkiyuet@gmail.com>
2023-08-11 15:16:52 +0800

    a

---
+++ build-hangman-game-html-javascript/Hangman Game/scripts/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "cat",
+        hint: "meow meow meow"
+    },
+    {
+        word: "pig",
+        hint: "oink oink oink"
+    },
+    {
+        word: "dog",
+        hint: "woof woof woof "
+    },
+    {
+        word: "bee",
+        hint: "buzz"
+    },
+    {
+        word: "cow",
+        hint: "moo"
+    },
+    ];
